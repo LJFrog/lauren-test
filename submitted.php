4c767bca9eda600996b99f909341ff9b8d09473b
Lauren Marie Jensen <jensenlm@colorado.edu>
2019-01-23 15:24:14 -0700

    added web form

---
+++ submitted.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+I see that you submitted a server called <?php echo $_POST["fqdn"]; ?>
+ belonging to <?php echo $_POST["client"]; ?>
+ that is <?php If ($_POST["active"]=="no") echo "not "; ?>active.
+ 
+ To return home click <a href="index.html">here</a>.
+
+</body>
+</html>
